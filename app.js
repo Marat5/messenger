d8f10803bb5cfdcffbdec74c83b7c4d67e6ef7da
Marat <m.mukhutdinov@agora.ru>
2020-10-12 10:39:47 +0800

    Изменен текст сообщения в консоли после запуска проекта

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const app = express();
 app.use(express.static(`${__dirname}/static`));
 
 app.listen(PORT, () => {
-  console.log(`Мой текст в логе после запуска ${PORT}!`);
+  console.log(`Проект запущен на порте ${PORT}`);
 });
