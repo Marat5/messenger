04b3503cf91c584260d88905b0694231b45712f6
Marat <m.mukhutdinov@agora.ru>
2021-01-27 12:21:29 +0800

    Удален закомментированный код

--- app.js
+++ app.js
@@ -11,26 +11,6 @@ app.listen(PORT, () => {
 	console.log(`Проект запущен на порте ${PORT}`);
 });
 
-// app.get('/login', (req, res) => {
-// 	res.sendFile('/static/index.html', { root: __dirname });
-// });
-
-// app.get('/registration', (req, res) => {
-// 	res.sendFile('/static/index.html', { root: __dirname });
-// });
-
-// app.get('/profile', (req, res) => {
-// 	res.sendFile('/static/index.html', { root: __dirname });
-// });
-
-// app.get('/error', (req, res) => {
-// 	res.sendFile('/static/index.html', { root: __dirname });
-// });
-
-// app.get('/chat', (req, res) => {
-// 	res.sendFile('/static/index.html', { root: __dirname });
-// });
-
 app.get('*', (req, res) => {
 	res.status(404).sendFile('/static/index.html', { root: __dirname });
 });
